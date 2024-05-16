98586735b31634bde49ab03fdb5d37e3320e71b6
Matheus <matheus.guedes.mg.m@gmail.com>
2024-05-16 20:26:50 -0300

    tela principal
    Há uma tela principal agora, ainda não tem funcionalidade alguma.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,76 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+/**
+ * Pergunta ao usuário um número inteiro repetidas vezes
+ * até receber um número inteiro.
+ */
+int pergunta_opcoes(char *pergunta) {
+	int resposta, status;
+
+	do {
+		puts(pergunta);
+		status = scanf("%d", &resposta);
+
+		/* scanf retorna o número de valores encontrados e
+		 * atribuídos. Se este valor for zero, o usuário não 
+		 * digitou um número.
+		 *
+		 * É importante verificar se o valor é zero para evitar
+		 * loops infinitos.
+		 */
+		if (status == 0) {
+			printf("Opção Inválida.\n");
+			setbuf(stdin, NULL);
+		}
+	} while (status == 0);
+
+	return resposta;
+}
+
+void tela_principal() {
+	int opcao;
+	bool sair = false;
+
+	do {
+		/* TODO: substituir o número 12 pela quantidade real
+		 * de produtos cadastrados.
+		 */
+		printf("Números de produtos cadastrados %d", 12);
+		putchar('\n');
+		printf("1: Cadastrar produto.\n");
+		printf("2: Listar todos os produtos.\n");
+		printf("3: Editar cadastro.\n");
+		printf("4: Salvar em arquivo CSV.\n");
+		printf("5: Sair.\n");
+		putchar('\n');
+
+		opcao = pergunta_opcoes("Escolha uma opção: ");
+
+		switch (opcao) {
+			case 1:
+				printf("\n\nNão implementado.\n");
+				break;
+			case 2:
+				printf("\n\nNão implementdado.\n");
+				break;
+			case 3:
+				printf("Não Implementado.\n");
+				break;
+			case 4:
+				printf("Não Implementado.\n");
+				break;
+			case 5:
+				sair = true;
+				break;
+			default:
+				printf("Opção Inválida.\n");
+				setbuf(stdin, NULL);
+
+		}
+	} while (!sair);
+}
+
 void tela_inicial() {
 	int opcao;
 	bool sair = false;
@@ -17,29 +87,14 @@ void tela_inicial() {
 		printf("3: Créditos.\n");
 		printf("4: Sair.\n");
 		putchar('\n');
-
-		printf("Escolha uma opção: ");
-		int status_entrada = scanf("%d", &opcao);
-
-		/* scanf retorna o número de valores encontrados e
-		 * atribuídos. Se este valor for zero, o usuário não 
-		 * digitou um número.
-		 *
-		 * É importante verificar se o valor é zero para evitar
-		 * loops infinitos.
-		 */
-		if (status_entrada == 0) {
-			printf("Opção Inválida.\n");
-			setbuf(stdin, NULL);
-			continue;
-		}
-
+		opcao = pergunta_opcoes("Escolha uma opção: ");
+		
 		switch(opcao) {
 			case 1:
 				printf("\nHAHAHA! Esta opção ainda não foi implementada >:3\n\n");
 				break;
 			case 2:
-				printf("\nNão implementado.\n\n");
+				tela_principal();
 				break;
 			case 3:
 				printf("\nSoftware desenvolvido pelos alunos:\nMatheus Ferreira Guedes\nGustavo Eklund França (programador mestre)\nPedro Juliani Gonçalves\n\n");
@@ -51,11 +106,11 @@ void tela_inicial() {
 				printf("Opção Inválida.\n");
 				setbuf(stdin, NULL);
 		}
-	} while(!sair);
+	} while (!sair);
 
 }
 
 int main() {
 	tela_inicial();
-		return 0;
+	return 0;
 }
